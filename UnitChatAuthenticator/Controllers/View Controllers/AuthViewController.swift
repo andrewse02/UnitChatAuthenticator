1d924b921fc5ed0d30922620770ae1016d85cd1e
Andrew Elliott <andrewelliott159@gmail.com>
2022-04-07 17:19:09 -0600

    Finished login functionality, displays username and image. Started on auth requests/responses

--- UnitChatAuthenticator/Controllers/View Controllers/AuthViewController.swift
+++ UnitChatAuthenticator/Controllers/View Controllers/AuthViewController.swift
@@ -8,9 +8,43 @@
 import UIKit
 
 class AuthViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var codeLabel: UILabel!
+    @IBOutlet weak var codeTextField: UITextField!
+    
+    // MARK: - Lifecycles
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
-
+    
+    // MARK: - Helper Functions
+    
+    func updateViews() {
+        guard let user = UserController.shared.user else { return }
+        
+        usernameLabel.text = "Hello, \(user.username)"
+        codeTextField.attributedPlaceholder = NSAttributedString(string: codeTextField.placeholder ?? "Enter Code", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
+        
+        codeTextField.isEnabled = false
+        codeTextField.isHidden = true
+        
+        // UIKit does not like SVGs :angry:
+        if let url = URL(string: user.profilePic.replacingOccurrences(of: ".svg", with: ".png")) {
+            URLSession.shared.dataTask(with: url) { [weak self] (data, _, _) in
+                if let data = data {
+                    DispatchQueue.main.async {
+                        self?.profileImage.layer.cornerRadius = (self?.profileImage.frame.size.height ?? 48) / 2
+                        self?.profileImage.clipsToBounds = true
+                        self?.profileImage.image = UIImage(data: data)
+                    }
+                }
+            }.resume()
+        }
+    }
 }
--- UnitChatAuthenticator/Controllers/View Controllers/LoginViewController.swift
+++ UnitChatAuthenticator/Controllers/View Controllers/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class LoginViewController: UIViewController {
 
@@ -19,6 +20,7 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateViews()
+        checkLoggedIn()
     }
     
     // MARK: - Actions
@@ -32,21 +34,15 @@ class LoginViewController: UIViewController {
         UsersNetworkManager.loginRequest(username: username, password: password) { result in
             switch result {
             case .success(let token):
-                TokenController.shared.token = Token(value: token)
-                UsersNetworkManager.fetchUser { result in
-                    switch result {
-                    case .success(let user):
-                        UserController.shared.user = user
-                        print(user)
-                        
-                        // Transition to second view
-                    case .failure(let error):
-                        DispatchQueue.main.async {
-                            print(error.errorDescription ?? error)
-                            self.presentErrorToUser(localizedError: error)
-                        }
-                    }
+                let fetchRequest = NSFetchRequest<Token>(entityName: "Token")
+                if let results = try? CoreDataStack.context.fetch(fetchRequest),
+                   let result = results.first {
+                    CoreDataStack.context.delete(result)
+                    CoreDataStack.saveContext()
                 }
+                
+                TokenController.shared.token = Token(value: token)
+                self.fetchUser()
             case .failure(let error):
                 DispatchQueue.main.async {
                     print(error.errorDescription ?? error)
@@ -61,5 +57,39 @@ class LoginViewController: UIViewController {
     func updateViews() {
         usernameTextField.attributedPlaceholder = NSAttributedString(string: usernameTextField.placeholder ?? "Username", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
         passwordTextField.attributedPlaceholder = NSAttributedString(string: passwordTextField.placeholder ?? "Password", attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0.25)])
+    }
+    
+    func checkLoggedIn() {
+        fetchToken()
+        
+        if TokenController.shared.token != nil {
+            fetchUser()
+        }
+    }
+    
+    func fetchToken() {
+        let fetchRequest = NSFetchRequest<Token>(entityName: "Token")
+        if let token = try? CoreDataStack.context.fetch(fetchRequest).first {
+            TokenController.shared.token = token
+        }
+    }
+    
+    func fetchUser() {
+        UsersNetworkManager.fetchUser { result in
+            switch result {
+            case .success(let user):
+                UserController.shared.user = user
+                DispatchQueue.main.async {
+                    let viewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "AuthViewController")
+                    viewController.modalPresentationStyle = .fullScreen
+                    self.present(viewController, animated: true)
+                }
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    print(error.errorDescription ?? error)
+                    self.presentErrorToUser(localizedError: error)
+                }
+            }
+        }
     }
 }
